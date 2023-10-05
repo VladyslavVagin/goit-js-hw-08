49dd669431887f9c8cf2e153dd20ee0473199abe
VladyslavVagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-10-06 00:25:03 +0200

    Task-02(completed)

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import vimeoPlayer from "@vimeo/player";
+var throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new vimeoPlayer(iframe);
+
+player.on('timeupdate', throttle(function (data) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data));
+}, 1000));
+
+const timingOfVideo = localStorage.getItem("videoplayer-current-time");
+const parsedTimingSeconds = JSON.parse(timingOfVideo).seconds;
+
+player.setCurrentTime(parsedTimingSeconds);
