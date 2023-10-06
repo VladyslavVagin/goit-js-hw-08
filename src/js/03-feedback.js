3eb93623f432ee74385cb6a90240509886698afd
VladyslavVagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-10-06 21:50:06 +0200

    Task-03(completed)

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+// import the THROTTLE
+var throttle = require('lodash.throttle');
+
+// SET CONST VALUES 
+const feedbackForm = document.querySelector('.feedback-form');
+const inputBox = document.querySelector('input');
+const message = document.querySelector('textarea');
+const FEEDBACK = "feedback-form-state";
+const formData = {};
+
+// ADD EVENT LISTENER FOR INPUT FORM + ADD INPUT DATA TO LOCAL STORAGE
+feedbackForm.addEventListener("input", throttle((e => {
+    const value = e.target.value;
+    formData[e.target.name] = value;
+    localStorage.setItem(FEEDBACK, JSON.stringify(formData));
+}), 500));
+
+// IF WEBSITE RENEW (AND NO SUBMIT!!!) ALL DATA REMAIN IN INPUT AREA
+showDataSaved();
+
+function showDataSaved() {
+    const savedInputData = localStorage.getItem(FEEDBACK);
+    console.log(savedInputData);
+    if (savedInputData) {
+        inputsDataSaved = JSON.parse(savedInputData);
+        inputBox.value = inputsDataSaved.email;
+        message.value = inputsDataSaved.message;
+    };
+};
+
+// SUMBIT FORM + RESET INPUTS + REMOVE FROM LOCAL STORAGE INPUT DATA
+feedbackForm.addEventListener("submit", e => {
+    e.preventDefault();
+    e.currentTarget.reset();
+    localStorage.removeItem(FEEDBACK);
+});
 
