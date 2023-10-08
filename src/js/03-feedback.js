2dff4498e55b02b449b549dd85c5e77ccd2d1d15
VladyslavVagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-10-08 09:09:51 +0200

    Task-03(correction)
    due to mentor advice

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,9 +4,9 @@ var throttle = require('lodash.throttle');
 // SET CONST VALUES 
 const feedbackForm = document.querySelector('.feedback-form');
 const inputBox = document.querySelector('input');
-const message = document.querySelector('textarea');
+const msg = document.querySelector('textarea');
 const FEEDBACK = "feedback-form-state";
-const formData = {};
+let formData = { email: '', message: '' };
 
 // ADD EVENT LISTENER FOR INPUT FORM + ADD INPUT DATA TO LOCAL STORAGE
 feedbackForm.addEventListener("input", throttle((e => {
@@ -20,18 +20,24 @@ showDataSaved();
 
 function showDataSaved() {
     const savedInputData = localStorage.getItem(FEEDBACK);
-    console.log(savedInputData);
     if (savedInputData) {
         const inputsDataSaved = JSON.parse(savedInputData);
         inputBox.value = inputsDataSaved.email;
-        message.value = inputsDataSaved.message;
+        msg.value = inputsDataSaved.message;
+        formData = { email: `${inputBox.value}`, message: `${msg.value}` };
     };
 };
 
 // SUMBIT FORM + RESET INPUTS + REMOVE FROM LOCAL STORAGE INPUT DATA
 feedbackForm.addEventListener("submit", e => {
     e.preventDefault();
-    e.currentTarget.reset();
-    localStorage.removeItem(FEEDBACK);
+    if (inputBox.value && msg.value) {
+        e.currentTarget.reset();
+        localStorage.removeItem(FEEDBACK);
+        console.log(formData);
+    } else {
+        alert("ATTENTION !!! PLEASE, ALL FIELDS SHOULD BE FILLED !!!!")
+    }
+
 });
 
